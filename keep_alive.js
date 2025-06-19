c6168dff4443ca8d47bf23795c0792c510f86d24
Kittitatx2 <76760183+Kittitatx2@users.noreply.github.com>
2025-06-19 23:16:04 +0700

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,15 @@
-var http = require('http');
+// keep_alive.js
+const express = require('express');
+const app = express();
 
-http.createServer(function (req, res) {
-  res.write("I'am alive");
-  res.end();
-}).listen(3000);
+app.all("/", (req, res) => {
+  res.send("✅ AFK Bot is alive!");
+});
+
+function keepAlive() {
+  app.listen(3000, () => {
+    console.log("✅ Server is running on port 3000");
+  });
+}
+
+module.exports = keepAlive;
